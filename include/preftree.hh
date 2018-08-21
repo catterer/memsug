464ae364dc10b4708a03582114ca6b2d1ff4aeeb
Arseniy Simonov <harvos.arsen@gmail.com>
2018-08-21 19:08:19 +0200

    tmp

--- include/preftree.hh
+++ include/preftree.hh
@@ -11,10 +11,16 @@ namespace preftree {
 using Digit = uint8_t;
 class Path: public std::list<Digit> {
 public:
-    Path(uint64_t val) {
+    Path(uint64_t val):
+        key_{val}
+    {
         for(; val; val /= 10)
             push_front(val%10);
     }
+
+    auto key() const -> uint64_t { return key_; }
+private:
+    uint64_t key_;
 };
 
 using Key = uint64_t;
@@ -22,9 +28,12 @@ using Key = uint64_t;
 template<typename VAL>
 class Node {
 public:
-    using Children = std::map<Digit, std::shared_ptr<Node>>;
+    using Children = std::map<Digit, std::shared_ptr<Node<VAL>>>;
 
-    Node(VAL&& val): val_{std::move(val)} {}
+    Node(std::weak_ptr<Node<VAL>> parent, VAL&& val):
+        parent_{parent},
+        val_{std::move(val)}
+    { }
 
     auto parent() const -> const Node* { return parent_; }
     auto parent() -> Node* { return parent_; }
@@ -39,7 +48,7 @@ public:
     auto children() -> Children& { return children_; }
 
 private:
-    std::weak_ptr<Node>     parent_{};
+    std::weak_ptr<Node<VAL>>    parent_{};
     Children                children_;
     VAL                     val_;
 };
@@ -59,7 +68,7 @@ public:
     preftree_iterator& operator--() { node_ = node_->parent(); }
     bool operator==(const preftree_iterator& other) const { return node_ == other.node_; }
     bool operator!=(const preftree_iterator& other) const { return !(*this == other); }
-    auto node() const -> std::weak_ptr<const Node<T>>& { return node_; }
+    auto node() const -> std::weak_ptr<Node<T>>& { return node_; }
 private:
     std::weak_ptr<Node<T>> node_{};
 };
@@ -72,10 +81,12 @@ public:
 
     Preftree() = default;
     auto emplace(Key, VAL&&) -> std::pair<bool, iterator>;
+    auto emplace(iterator tit, const Path& path, Path::const_iterator pit, VAL&& v) -> std::pair<bool, iterator>;
 
     auto find(Key) -> iterator;
     auto find(Key) const -> const_iterator;
 
+    auto find_closest(const Path&, Path::const_iterator&) -> iterator;
     auto find_closest(const Path&, Path::const_iterator&) const -> const_iterator;
     auto find_closest(const Path&, Path::const_iterator&, const_iterator start) const -> const_iterator;
 
@@ -83,12 +94,43 @@ public:
     auto cbegin() -> const_iterator { return const_iterator(root_); }
 
     auto end() -> iterator { return iterator(nullptr); }
-    auto cend() -> const_iterator { return const_iterator(nullptr); }
+    auto cend() const -> const_iterator { return const_iterator(nullptr); }
 
 private:
-    std::shared_ptr<Node<VAL>> root_;
+    std::shared_ptr<Node<VAL>> root_{std::make_shared<Node<VAL>>(Node<VAL>({}, {}))};
 };
 
+
+template<typename VAL>
+auto Preftree<VAL>::emplace(Key k, VAL&& v)
+    -> std::pair<bool, iterator>
+{
+    auto path = Path(k);
+    auto pit = path.cbegin();
+    auto ti = find_closest(path, pit);
+    return emplace(ti, path, pit, std::move(v));
+}
+
+template<typename VAL>
+auto Preftree<VAL>::emplace(iterator tit, const Path& path, Path::const_iterator pit, VAL&& v)
+    -> std::pair<bool, iterator>
+{
+    if (pit == path.cend())
+        return std::make_pair(false, tit);
+
+    auto& children = tit.node().children();
+    auto pr = children.emplace(*pit, std::make_shared<Node<VAL>>(tit.node(), std::move(v)));
+    assert(pr.first); // bug in find_closest
+    return std::make_pair(true, emplace(iterator(pr.second), path, ++pit, std::move(v)).second);
+}
+
+template<typename VAL>
+auto Preftree<VAL>::find_closest(const Path& p, Path::const_iterator& pit)
+    -> iterator
+{
+    auto const_tit = find_closest(p, pit, cbegin());
+    return iterator(const_tit.node());
+}
 
 template<typename VAL>
 auto Preftree<VAL>::find_closest(const Path& p, Path::const_iterator& pit) const
--- src/preftree.test.cc
+++ src/preftree.test.cc
@@ -1,8 +1,15 @@
 #include <preftree.hh>
 #include <string>
+#include <gtest/gtest.h>
+
+using namespace preftree;
+
+TEST(Preftree, basic) {
+    Preftree<std::string> pt{};
+    pt.emplace(1, "1");
+    ASSERT_EQ(*pt.find(1), "1");
+}
 
 int main(int argc, char** argv) {
-    using namespace preftree;
-    Preftree<std::string> pt{};
-    return 0;
+    return RUN_ALL_TESTS();
 }
