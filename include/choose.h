def4050e2180f86ad634f40e1702c214d8698617
Arseniy Simonov <ars@exasol.com>
2018-08-22 10:14:28 +0200

    forgot choose.h

---
+++ include/choose.h
@@ -0,0 +1,14 @@
+#pragma once
+
+template <bool flag, class IsTrue, class IsFalse>
+struct choose;
+
+template <class IsTrue, class IsFalse>
+struct choose<true, IsTrue, IsFalse> {
+   typedef IsTrue type;
+};
+
+template <class IsTrue, class IsFalse>
+struct choose<false, IsTrue, IsFalse> {
+   typedef IsFalse type;
+};
