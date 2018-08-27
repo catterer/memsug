b6c2126a57d2fe80d02f645bd06124d1bb062f28
Arseniy Simonov <ars@exasol.com>
2018-08-27 17:14:50 +0200

    more tests

--- include/memsug.test.hh
+++ include/memsug.test.hh
@@ -19,7 +19,7 @@ TEST_F(Donsug, simple) {
     ASSERT_EQ(*suger_->suggest_word("9167"), "батюшка");
 }
 
-TEST_F(Donsug, mwl) {
+TEST_F(Donsug, mwl_variants) {
     auto res = suger_->maximize_word_length("12");
     ASSERT_EQ(res->size(), 1U);
     const auto vs = *res->begin();
@@ -27,6 +27,17 @@ TEST_F(Donsug, mwl) {
     auto sorted = std::accumulate(vs.begin(), vs.end(), std::set<std::string>(),
             [] (auto&& accum, const auto& word) { accum.emplace(word); return std::move(accum); });
     ASSERT_EQ(sorted, (std::set<std::string>{"Дона", "Дону", "Дон", "дна"}));
+}
+
+TEST_F(Donsug, mwl_length) {
+    auto res = suger_->maximize_word_length("03567002");
+    ASSERT_EQ(res->size(), 2U);
+    auto vsi = res->begin();
+    ASSERT_EQ(vsi->size(), 1U);
+    ASSERT_EQ(*vsi->begin(), "землюшка");
+    ++vsi;
+    ASSERT_EQ(vsi->size(), 1U);
+    ASSERT_EQ(*vsi->begin(), "засеяна");
 }
 
 }
