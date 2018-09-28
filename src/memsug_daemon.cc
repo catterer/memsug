688309ee97fd4078392a5f6e87679c9fcb196f3a
Arseniy Simonov <ars@exasol.com>
2018-09-28 15:20:51 +0200

    better

--- src/memsug_daemon.cc
+++ src/memsug_daemon.cc
@@ -9,33 +9,32 @@
 namespace po = boost::program_options;
 
 using Config = boost::property_tree::ptree;
+using Evhttp = std::shared_ptr<evhttp>;
 
 class Server {
 public:
     Server(const std::string& config_path)
     {
         boost::property_tree::ini_parser::read_ini(config_path, cfg_);
+        if (!event_init())
+            throw std::runtime_error("failed to init libevent");
+
+        auto host = cfg_.get("bind.host", "localhost");
+        auto port = cfg_.get("bind.port", 8080);
+        evhttp_ = std::shared_ptr<evhttp>(evhttp_start(host.c_str(), port), evhttp_free);
+        if (!evhttp_)
+            throw std::runtime_error("failed to init http server");
     }
 
     void run();
 
 private:
     Config cfg_;
+    Evhttp evhttp_;
 };
 
 void Server::run() {
-    if (!event_init())
-        throw std::runtime_error("failed to init libevent");
-
-    auto host = cfg_.get("bind.host", "localhost");
-    auto port = cfg_.get("bind.port", 8080);
-    std::cout << host << ":" << port << "\n";
-    std::unique_ptr<evhttp, decltype(&evhttp_free)> server(evhttp_start(host.c_str(), port), &evhttp_free);
-
-    if (!server)
-        throw std::runtime_error("failed to init http server");
-
-    evhttp_set_gencb(server.get(), [] (evhttp_request *req, void *) {
+    evhttp_set_gencb(evhttp_.get(), [] (evhttp_request *req, void *) {
             auto *reply = evhttp_request_get_output_buffer(req);
             if (!reply)
                 return;
