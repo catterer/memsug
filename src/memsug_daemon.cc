0d9fe4fe2a231713262102a3f58e8a3012ae44da
Arseniy Simonov <ars@exasol.com>
2018-09-28 14:50:52 +0200

    config

--- src/memsug_daemon.cc
+++ src/memsug_daemon.cc
@@ -2,14 +2,35 @@
 #include <cstdint>
 #include <iostream>
 #include <evhttp.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/program_options.hpp>
 
-int main(int argc, char** argv) try {
+namespace po = boost::program_options;
+
+using Config = boost::property_tree::ptree;
+
+class Server {
+public:
+    Server(const std::string& config_path)
+    {
+        boost::property_tree::ini_parser::read_ini(config_path, cfg_);
+    }
+
+    void run();
+
+private:
+    Config cfg_;
+};
+
+void Server::run() {
     if (!event_init())
         throw std::runtime_error("failed to init libevent");
 
-    auto host = "127.0.0.1";
-    auto port = 5555;
-    std::unique_ptr<evhttp, decltype(&evhttp_free)> server(evhttp_start(host, port), &evhttp_free);
+    auto host = cfg_.get("bind.host", "localhost");
+    auto port = cfg_.get("bind.port", 8080);
+    std::cout << host << ":" << port << "\n";
+    std::unique_ptr<evhttp, decltype(&evhttp_free)> server(evhttp_start(host.c_str(), port), &evhttp_free);
 
     if (!server)
         throw std::runtime_error("failed to init http server");
@@ -24,7 +45,32 @@ int main(int argc, char** argv) try {
 
     if (event_dispatch() == -1)
         throw std::runtime_error("failed to run event loop");
+}
 
+int main(int argc, char** argv) try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("config,c", po::value<std::string>(), "config file")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 0;
+    }
+
+    if (not vm.count("config")) {
+        std::cout << "no config specified\n";
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    Server srv(vm["config"].as<std::string>());
+    srv.run();
     return 0;
 } catch (const std::exception& x) {
     std::cerr << x.what() << "\n";
